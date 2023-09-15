eea8940057a0e71a48e431f6502dd6f2d186d3a8
hassanDurra <hassan2109f@aptechgdn.net>
2023-09-15 04:10:21 -0700

    Migrations added

--- Cosmatic/laravel_10/database/migrations/2014_10_12_000000_create_users_table.php
+++ Cosmatic/laravel_10/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('role')->default(0);
+            $table->string('contact');
+            $table->string('profileimage');
             $table->string('password');
+            $table->integer('age');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
