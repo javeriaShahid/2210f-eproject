e79f2b6595f7a75c4d0f641765ba141d89f3dc37
hassanDurra <hassan2109f@aptechgdn.net>
2023-09-26 02:06:32 +0500

    admin seeder fixed

--- database/seeders/Adminseeder.php
+++ database/seeders/Adminseeder.php
@@ -15,7 +15,7 @@ class Adminseeder extends Seeder
         $email_verified_at = date('Y-m-d H:i:s', strtotime('now'));
         \DB::table('users')->insert(
             array(
-               'name'=> 'Hassan Murad' , 'username' => 'hassanMurad!223' ,'email' =>'hassan2109f@aptechgdn.net' ,'password' => bcrypt(12345678) ,'is_blocked' => 0 ,'email_verified_at' => $email_verified_at , 'contact_number' => '3030239865' , 'phone_code' => '+92' , 'role' => 0 , 'status' => 0
+               'name'=> 'Hassan Murad' , 'username' => 'hassanMurad!223' ,'email' =>'hassan2109f@aptechgdn.net' ,'password' => bcrypt(12345678) ,'is_blocked' => 0 ,'email_verified_at' => $email_verified_at , 'contact_number' => '3030239865' , 'phone_code' => '+92' , 'role' => 1 , 'status' => 0
             ) ,
             array(
 
