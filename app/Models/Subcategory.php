fa05b79ddad7f7303b480ce0df3684376c313ee0
hassanDurra <hassan2109f@aptechgdn.net>
2023-09-25 18:00:34 +0500

    Basic Eloquent Query

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -14,4 +14,9 @@ class Subcategory extends Model
         'name' ,
         'deleted_at'  ,
       ];
+
+    public function category() // Ye Ek Basic eloquent query hai jese hum ek table ya model ko dosre se connect karenge ok
+    {
+        return $this->hasMany(Category::class , 'id' , 'category_id'); // ab es query mai jo $this haiwoh hamara current model hai hum esme hasMany ka use karhe hai means esme Ek se zeyada same category id ke subcategories hone fir Andar hum os Class ya Model ko call krhe hai or kehrhe hain ke eska jo Id hoga  woh hamare es column se match karega jo ke hamara category_id hai ok
+    }
 }
