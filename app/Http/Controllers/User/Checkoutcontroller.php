7c9f0d9de62632c8985cf5fe6559ad0f76635635
javeriaShahid <120173660+javeriaShahid@users.noreply.github.com>
2023-09-25 20:28:43 +0500

    seeders add

--- app/Http/Controllers/User/Checkoutcontroller.php
+++ app/Http/Controllers/User/Checkoutcontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Cartcontroller extends Controller
+class Checkoutcontroller extends Controller
 {
     //
 }
