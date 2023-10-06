2cc4f9de469545c085648663722f539eb8ffed99
Muskanyousuf2210E <javeria2210e@aptechgdn.net>
2023-10-07 02:36:04 +0500

    orderrrrrplacedpg

---
+++ resources/views/User/orderplaced.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+
+    <title>thankyou!</title>
+</head>
+
+<body>
+
+    <div>
+        <div class="container "
+            style="margin-top: 50px; height: 100px;  width: 100px; ">
+            <!-- <img width="98" height="98" src="https://img.icons8.com/color/96/checkmark--v1.png" alt="checkmark--v1" /> -->
+            <img src="C:\Users\Win 10\Downloads\icons8-check-500.png" height="100px" width="100px">
+        </div>
+        <h2 class="d-flex justify-content-center" style="color: green;">Order Successfully Placed</h2>
+    </div>
+
+    <p style="text-align: center; margin-top: 50px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
+        asperiores quam soluta ratione excepturi atque iure nihil ea, voluptatum
+        doloribus perferendis eos illum quod!</p>
+    <div class="container">
+        <div class="table-wrapper">
+            <table class="eg-table order-table table mb-0">
+                <thead>
+                    <tr class=" bg-dark text-white">
+                        <th>Image</th>
+                        <th>Order ID</th>
+                        <th>Product Details</th>
+                        <th>price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-label="Image"><img alt="image" src="assets/img/inner-page/whistlist-img1.png"
+                                class="img-fluid">
+                        </td>
+                        <td data-label="Order ID">#4ce345c3e</td>
+                        <td data-label="Product Details">Eau De Blue Perfume</td>
+                        <td data-label="price">$40.00</td>
+                    </tr>
+                    <tr>
+                        <td data-label="Image"><img alt="image" src="assets/img/inner-page/whistlist-img2.png"
+                                class="img-fluid">
+                        </td>
+                        <td data-label="Order ID">#4ce3533e</td>
+                        <td data-label="Product Details">Smooth Makeup Box</td>
+                        <td data-label="price">$25.00</td>
+                    </tr>
+                    <tr>
+                        <td data-label="Image"><img alt="image" src="assets/img/inner-page/whistlist-img3.png"
+                                class="img-fluid">
+                        </td>
+                        <td data-label="Order ID">#8ce3533e</td>
+                        <td data-label="Product Details">Modern Red Lipstick </td>
+                        <td data-label="price">$32.00</td>
+                    </tr>
+                    <tr>
+                        <td data-label="Image"><img alt="image" src="assets/img/inner-page/whistlist-img4.png"
+                                class="img-fluid">
+                        </td>
+                        <td data-label="Order ID">#8ce3533e</td>
+                        <td data-label="Product Details">New Botanical Shampoo</td>
+                        <td data-label="price">$27.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="container" style="margin-top: 50px;">
+        <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li> Nemo sit, facere ipsum soluta nulla commodi
+                quos laborum distinctio explicabo accusantium.</li>
+        </ul>
+    </div>
+    <button
+        style="background-color: orangered ; border-radius: 10px; border-color: orangered; color: aliceblue;">Download
+        Reciept</button>
+    <div>
+
+    </div>
+</body>
+
+</html>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
